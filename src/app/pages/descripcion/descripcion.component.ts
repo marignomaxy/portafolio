c4b09ac6b680a4be2d0c99c9d491d442831e20bd
Maximiliano Marigno <marignomaxy@gmail.com>
2024-06-01 11:43:46 -0300

    modificacion es descripcion

--- src/app/pages/descripcion/descripcion.component.ts
+++ src/app/pages/descripcion/descripcion.component.ts
@@ -2,17 +2,19 @@ import { Component } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 import { Paginas } from '../../interfaces/paginas'
 import { PaginasService } from '../../services/paginas.service'
+import { LoaderComponent } from '../../components/loader/loader.component'
 
 @Component({
   selector: 'app-descripcion',
   standalone: true,
-  imports: [],
   templateUrl: './descripcion.component.html',
   styleUrl: './descripcion.component.scss',
+  imports: [LoaderComponent],
 })
 export class DescripcionComponent {
   id!: number
   pagina!: Paginas
+  loading: boolean = true
 
   constructor(private route: ActivatedRoute, private paginaServices: PaginasService) {
     this.route.params.subscribe((params) => {
@@ -20,6 +22,9 @@ export class DescripcionComponent {
     })
     this.paginaServices.getPaginaByID(this.id).subscribe((pagina: any) => {
       this.pagina = pagina
+      setTimeout(() => {
+        this.loading = false
+      }, 1000)
     })
   }
 }
